4ddf1af777053a6e8fbcccfd424ee938bd53a364
vinay-pythonista <vinay.choudhary1994@outlook.com>
2020-09-16 21:44:00 +0530

    Checkout page and Subtotal component

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,9 @@ export const initialState = {
     user: null,
 };
 
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
     console.log(action)
     switch(action.type) {
@@ -13,8 +16,24 @@ const reducer = (state, action) => {
             }
             break;
         case 'REMOVE_FROM_BASKET':
-            return { state }
-            break;
+            let newBasket = [...state.basket]
+
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+            if(index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove product (id: ${action.id}) as its not in the basket!`
+                );
+            }
+            
+            return { 
+                ...state,
+                basket: newBasket
+            }
         default:
             return state;
     }
