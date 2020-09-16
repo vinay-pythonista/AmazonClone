33ceed0a0d8a350d53f61bf88aff0a277b4026cb
vinay-pythonista <vinay.choudhary1994@outlook.com>
2020-09-16 13:28:34 +0530

    Create Banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from './Home'
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+          <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -16,7 +18,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>Home page</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
